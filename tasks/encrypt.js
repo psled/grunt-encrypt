98579294911ed8e35bf9f27361457d22ef0f9f40
Pawel Sledzikowski <pawel.sledzikowski@gmail.com>
2014-11-05 23:16:35 +0100

    minor fixes in encrypt task

--- tasks/encrypt.js
+++ tasks/encrypt.js
@@ -14,36 +14,41 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('encrypt', 'The best Grunt plugin ever.', function() {
+  grunt.registerMultiTask('encrypt', 'The best Grunt plugin ever.', function() {    
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options();
-    if (!options.key) {
+    var opts = this.options();
+    if (!opts.key) {
       grunt.fail.warn('Missing key property.');
     }
-    var key = options.key;
+    var key = opts.key;
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
       // Concat specified files.
-      var src = f.src.filter(function(filepath) {
+      var src = f.src.filter(function(filepath) {        
         // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
           return false;
         } else {
-          var filename = path.basename(filepath);
-          var newFilename = (grunt.file.isDir(options.dest) ? [filepath, 'encrypted'].join('.') : (options.ext) ? [options.dest, options.ext].join('.') : options.dest);
+          var filename = path.basename(filepath, path.extname(filepath));
+          var newDirname = path.join(opts.dest, path.dirname(filepath));
+          var newFilename = [
+            path.join(newDirname, filename),
+            opts.ext ? opts.ext : 'encrypted'
+          ].join('.'); 
+
           var contents = grunt.file.read(filepath);
 
-          if (!options.decrypt) {
+          if (!opts.decrypt) {
             var cipher = crypto.createCipher('aes-256-cbc', key)
-            cipher.update(contents, 'utf8', 'base64');
-            contents = cipher.final('base64')
+            contents = cipher.update(contents, 'utf8', 'base64');
+            contents += cipher.final('base64')
           }
           else {
             var decipher = crypto.createDecipher('aes-256-cbc', key);
-            decipher.update(contents, 'base64', 'utf8');
-            contents = decipher.final('utf8');
+            contents = decipher.update(contents, 'base64', 'utf8');
+            contents += decipher.final('utf8');
             var ext = filepath.split('.');
             ext = ext[ext.length - 1];
             newFilename = newFilename.split('.' + ext).join('');
